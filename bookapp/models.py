5a55a19ad7a4a7300973a4eb6892db46531ce24d
shakyasimha <sushakya235@gmail.com>
2023-06-09 09:08:57 +0545

    made migrations and updated models

--- bookapp/models.py
+++ bookapp/models.py
@@ -11,3 +11,9 @@ class Book(models.Model):
         title = models.CharField(max_length=200)
         author = models.CharField(max_length=200)
         pub_year = models.IntegerField(default=0)
+    
+    # Method for taking input the data
+    def __input__(self, title, author, pub_year):
+        self.title    = title 
+        self.author   = author 
+        self.pub_year = pub_year
