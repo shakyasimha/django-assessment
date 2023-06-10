49daf800876e99bfd69109caa265c1a89b895814
shakyasimha <sushakya235@gmail.com>
2023-06-10 14:57:32 +0545

    added forms and template layout

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from bookapp.forms import BookForm 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the homepage of book app")
+    return render(request, 'bookapp/home.html')
