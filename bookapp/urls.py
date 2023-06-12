22d16afe9e3fc7e7e0f8d8aa768da2f81f008f33
shakyasimha <sushakya235@gmail.com>
2023-06-12 09:58:01 +0545

    changed urls and views

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
 
-from .views import BookView
+from .views import book_list, book_detail, book_search
 
 app_name = 'bookapp'
 
 urlpatterns = [
-    path('', BookView.as_view(), name='book_list'),
-    path('book_detail/<int:pk>/', BookView.as_view(), name='book_detail')
+    path('books/', book_list, name='book_list'),
+    path('books/<int:pk>/', book_detail, name='book-detail'),
+    path('books/search/', book_search, name='book-search')
 ]
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,58 +1,75 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404, HttpResponse
 from django.views import View
-from .forms import BookForm 
-from .models import Book 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models import Q 
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser 
 
+from .models import Book 
 
 """
     Template names, just for reference:
 
     - book_list.html 
     - book_detail.html 
-    - book_create.html
+    - book_search.html
 
 """
 
-class BookView(View):
-    template_name = 'book_list.html'
-    #template_name = 'book_detail.html'
-    form_class = BookForm 
+## Serializer class here
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book 
+        fields = ['id', 'title', 'author', 'publication_year']
 
-    ## For retrieving book entries
-    def get(self, request, pk=None):
-        if pk:
-            book = get_object_or_404(Book, pk=pk)
-            return render(request, 'book_detail.html', {'book': book})
+
+@csrf_exempt
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(book, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
         
-        books = Book.objects.all()
-        return render(request, self.template_name, {'books': books})
-
-
-    ## For creating new book entries
-    def post(self, request, pk=None):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-        else:
-            return render(request, 'book_create.html', {'form': form})
+        return JsonResponse(serializer.errors, status=400)
     
-
-    ## For updating book entries
-    def put(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        form = self.form_class(request.POST, instance=book)
-
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-        else:
-            return render(request, 'book_update.html', {'form': form, 'book': book})
-    
-
-    ## For deleting the book entry
-    def delete(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
+    elif request.method == 'DELETE':
         book.delete()
-        return redirect('book_list.html')
+        return HttpResponse(status=400)
+
+
+@csrf_exempt
+def book_list(request):
+    if request.method == 'GET':
+        books = Book.object.all()
+        serializer = BookSerializer(books, safe=True)
+
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        
+        return JsonResponse(serializer.data, status=400)
+    
+
+@csrf_exempt
+def book_search(request):
+    if request.method == 'GET':
+        query = request.GET.get('q', '')
+        books = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query))
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse(serializer.data, safe=False)
